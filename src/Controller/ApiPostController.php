727fed819da23cf748e693104845ee7027db37e7
julie-j-code <jeannet.julie@gmail.com>
2020-06-02 20:02:01 +0100

    refactorisation return d'ApiPostController et ajout des annotations $Groups à Comment.php

--- src/Controller/ApiPostController.php
+++ src/Controller/ApiPostController.php
@@ -3,11 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\PostRepository;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-// use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
-// use Symfony\Component\HttpFoundation\JsonResponse;
-// use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiPostController extends AbstractController
@@ -17,18 +13,14 @@ class ApiPostController extends AbstractController
      */
     public function index(PostRepository $postRepository)
     {
-        $posts = $postRepository->findAll(); 
-
         /*         
         Au lieu d'utiliser le SerializerInterface (qui venait se substituer au NormalizerInterface) 
-        $json = $serializer->serialize($posts, 'json', ['groups' => 'posts:read']);
         et JsonResponse (qui venait se substituer à Response )
         $response = new JsonResponse($json, 200, [], true);
-        je vais utiliser une fonction héritée d'AbstractController
+        j'utilise LA fonction json héritée d'AbstractController
          */
 
-        $response = $this->json($posts, 200, [], ['groups' => 'posts:read'] );    
+        return $this->json($postRepository->findAll(), 200, [], ['groups' => 'posts:read'] );    
 
-        return $response;
     }
 }
